cf276bb24ba9b46f78e59604f180c1064143d5d3
baoduy <thaibaoduy.5724@gmail.com>
2025-05-29 02:52:17 +0700

    CRUD create users

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,16 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'name không được để trống nhá mày' })
+  name: string;
+
+  @IsNotEmpty({ message: 'email không được để trống nhá mày' })
+  @IsEmail({}, { message: 'email không đúng định dạng nhá mày' })
+  email: string;
+
+  @IsNotEmpty({ message: 'password không được để trống nhá mày' })
+  password: string;
+  phone: string;
+  address: string;
+  image: string;
+}
